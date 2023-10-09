c504526566269e237c0aca806e4c395dc4eff4a4
Andrea Damiani <andrea.dam90@gmail.com>
2023-10-09 21:15:23 +0200

    Euro rimanenti per il prossimo buono

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
-    <main class="h-[100dvh]">
-        <div class="my-10 flex items-center justify-between px-6 text-6xl">
-            <span class="text-xl font-semibold">Buoni: {{ totBuoni }}</span>
+    <main class="h-[100dvh] bg-neutral-50 dark:bg-neutral-950 dark:text-neutral-50">
+        <div class="flex items-center justify-between px-6 py-10 text-6xl">
+            <div class="flex flex-col gap-1 text-xl font-semibold">
+                <span>Buoni: {{ totBuoni }}</span>
+                <span>Al prossimo buono: {{ spreco }}€</span>
+            </div>
             <span>{{ result.toFixed(2) }}</span>
             <span>€</span>
         </div>
@@ -10,25 +13,26 @@
             <input
                 v-model.number="newItem"
                 ref="input"
-                name="prezzo"
                 id="prezzo"
                 placeholder="0.00 €"
-                class="rounded-xl border-2 p-4 text-4xl shadow-inner" />
+                class="rounded-xl border-2 p-4 text-end text-4xl text-neutral-950 shadow-inner" />
 
-            <button class="mt-5 w-1/2 self-center rounded-lg bg-red-600 py-2 text-2xl text-white" type="reset">Reset</button>
+            <button class="mt-5 w-1/2 self-center rounded-lg bg-red-600 py-2 text-2xl text-white shadow-md" type="reset">Reset</button>
         </form>
     </main>
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
 import { computedEager } from "@vueuse/core";
 
 const newItem = ref();
 const result = ref(0);
 const totBuoni = computedEager(() => Math.floor(result.value / 8));
-const spreco = computedEager(() => result.value - totBuoni.value);
+const spreco = computed(() => (8 - (result.value % 8)).toFixed(2));
 
+// const { history, undo, redo } = useRefHistory(result);
+// console.log(history.value);
 const addItem = () => {
     result.value += newItem.value;
     newItem.value = null;
