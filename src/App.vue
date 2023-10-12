a2028cbd1b4756fbbf615f34093781fdc69b5d05
Andrea Damiani <andrea.dam90@gmail.com>
2023-10-12 10:53:26 +0200

    App funzionante

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-    <main class="h-[100dvh] bg-neutral-50 dark:bg-neutral-950 dark:text-neutral-50">
+    <main>
         <div class="flex items-center justify-between px-6 py-10 text-6xl">
             <div class="flex flex-col gap-1 text-xl font-semibold">
-                <span>Buoni: {{ totBuoni }}</span>
+                <span>Buoni utilizzabili: {{ totBuoni }}</span>
                 <span :class="coloreSpreco">Al prossimo buono: {{ spreco }}€</span>
             </div>
-            <span>{{ resultVisualizzato }}</span>
+            <span>{{ result }}</span>
             <span>€</span>
         </div>
 
-        <form @submit.prevent="addItem" @reset.prevent="reset" class="flex w-full flex-col justify-center px-10">
+        <form @submit.prevent="addItem(counter)" @reset.prevent="reset" class="flex w-full flex-col justify-center px-10">
             <input
                 v-model.number="newItem"
                 ref="input"
@@ -23,11 +23,19 @@
             </div>
         </form>
 
-        <ul class="mt-10 flex flex-col gap-3 px-20 text-3xl">
-            <li v-for="(articolo, index) in articoliInseriti" :key="articolo" class="flex justify-between">
-                <span>{{ articolo.toFixed(2) }}</span>
-                <Icon role="button" @click="removeItem(articolo, index)" icon="tabler:circle-x-filled" class="text-4xl text-red-500" />
-            </li>
+        <ul class="flex flex-col gap-3 px-20 py-10 text-3xl relative">
+            <TransitionGroup
+                enter-from-class="opacity-0 transform-translate-y-10"
+                leave-to-class="opacity-0 transform-x-20"
+                leave-active-class="absolute">
+                <li
+                    v-for="(articolo, index) in articoliInseriti"
+                    :key="articolo + 1"
+                    class="flex justify-between transition-all duration-300 ease-out">
+                    <span>{{ articolo.toFixed(2) }} €</span>
+                    <Icon role="button" @click="removeItem(index)" icon="tabler:circle-x-filled" class="text-4xl text-red-500" />
+                </li>
+            </TransitionGroup>
         </ul>
     </main>
 </template>
@@ -38,11 +46,13 @@ import { Icon } from "@iconify/vue";
 import { computedEager, useLocalStorage, useRefHistory } from "@vueuse/core";
 
 const newItem = ref();
-const result = useLocalStorage("result", 0);
-const resultVisualizzato = computed(() => result.value.toFixed(2));
+const articoliInseriti = useLocalStorage("array-articoli", []);
+const initialValue = 0;
+const result = computed(() =>
+    articoliInseriti.value.reduce((accumulator, currentValue) => accumulator + currentValue, initialValue).toFixed(2)
+);
 const totBuoni = computedEager(() => Math.floor(result.value / 8));
 const spreco = computed(() => (8 - (result.value % 8)).toFixed(2));
-const articoliInseriti = ref([]);
 
 const coloreSpreco = computed(() => {
     if (spreco.value > 7 && spreco.value <= 8) return "text-green-500";
@@ -55,25 +65,21 @@ const coloreSpreco = computed(() => {
     if (spreco.value > 0 && spreco.value <= 1) return "text-red-500";
 });
 
-const { undo } = useRefHistory(result);
+const input = ref();
+onMounted(() => input.value.focus());
 
 const addItem = () => {
-    result.value += newItem.value;
     articoliInseriti.value.unshift(newItem.value);
     newItem.value = null;
 };
 
-const input = ref();
-onMounted(() => input.value.focus());
+const removeItem = (indice) => (articoliInseriti.value = articoliInseriti.value.filter((_, index) => index !== indice));
+
+const { undo } = useRefHistory(articoliInseriti, { deep: true });
 
 const reset = () => {
     if (confirm("Sei sicuro di voler resettare la spesa?")) {
-        result.value = 0;
+        articoliInseriti.value = [];
     }
-};
-
-const removeItem = (item) => {
-    result.value -= item;
-    articoliInseriti.value = articoliInseriti.value.filter(element => element !== item);
 };
 </script>
