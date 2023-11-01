a6b9625504ecf10c0526680d7075f3da3da23bcc
Andrea Damiani <andrea.dam90@gmail.com>
2023-11-01 12:36:29 +0100

    Implementata possibilità di inserire nome articolo

--- src/App.vue
+++ src/App.vue
@@ -31,13 +31,23 @@
         </Transition>
 
         <form @submit.prevent="addItem" @reset.prevent="reset" class="flex w-full flex-col justify-center px-10">
-            <input
-                v-model.number="newItem"
-                ref="input"
-                id="prezzo"
-                placeholder="0.00 €"
-                class="rounded-lg border-2 p-4 text-end text-4xl text-neutral-950 shadow-inner"
-                required />
+            <template v-if="nameInserted">
+                <input
+                    v-model.number="newItem.price"
+                    ref="priceInput"
+                    id="prezzo"
+                    placeholder="Inserisci prezzo"
+                    class="rounded-lg border-2 p-4 text-end text-4xl text-neutral-950 shadow-inner"
+                    required />
+            </template>
+            <template v-else>
+                <input
+                    v-model="newItem.name"
+                    ref="nameInput"
+                    id="articolo"
+                    placeholder="Inserisci nome articolo"
+                    class="rounded-lg border-2 p-4 text-end text-4xl text-neutral-950 shadow-inner" />
+            </template>
 
             <div class="mt-5 flex w-full justify-evenly text-2xl font-semibold text-white">
                 <BaseButton type="button" class="bg-yellow-400" @click="undo">Annulla</BaseButton>
@@ -46,44 +56,52 @@
         </form>
 
         <template v-if="articoliInseriti.length">
-            <span class="my-5 flex justify-center text-xl">Totale articoli: {{ articoliInseriti.length }}</span>
             <ul
-                class="relative mx-10 flex max-h-64 flex-col gap-3 overflow-y-auto rounded-xl border bg-neutral-100 px-10 py-5 text-3xl shadow-inner transition-all duration-300 ease-out dark:bg-neutral-800">
+                class="relative mx-10 my-5 flex max-h-64 flex-col gap-3 overflow-y-auto rounded-xl border bg-neutral-100 px-10 py-5 text-3xl shadow-inner transition-all duration-300 ease-out dark:bg-neutral-800">
                 <TransitionGroup
                     enter-from-class="opacity-0 -translate-y-10"
                     leave-to-class="opacity-0 -translate-x-20"
                     enter-active-class="transition-all duration-300 ease-out"
                     leave-active-class="absolute left-10 right-10 transition-all duration-300 ease-out">
                     <li v-for="articolo in articoliInseriti" :key="articolo.id" class="flex justify-between">
-                        <span class="w-24 text-right">{{ convertToCurrency(articolo.quantity) }}</span>
+                        <div class="flex gap-5">
+                            <span class="w-24 text-right">{{ convertToCurrency(articolo.price) }}</span>
+                            <span class="">{{ articolo.name }}</span>
+                        </div>
                         <Icon role="button" @click="removeItem(articolo.id)" icon="tabler:circle-x-filled" class="text-4xl text-red-500" />
                     </li>
                 </TransitionGroup>
             </ul>
+            <span class="my-5 flex justify-center text-xl">Numero articoli: {{ articoliInseriti.length }}</span>
         </template>
     </main>
 </template>
 
 <script setup>
-import { ref, onMounted, computed } from "vue";
+import { ref, onMounted, computed, reactive, nextTick } from "vue";
 import { Icon } from "@iconify/vue";
-import { computedEager, useLocalStorage, useRefHistory, useToggle } from "@vueuse/core";
+import { useLocalStorage, useRefHistory, useToggle } from "@vueuse/core";
 
 import BaseButton from "./components/BaseButton.vue";
 
-const newItem = ref();
+const newItem = reactive({
+    name: "",
+    price: null,
+});
 const articoliInseriti = useLocalStorage("array-articoli", []);
 const counter = useLocalStorage("counter", 0);
-const result = computed(() => articoliInseriti.value.reduce((acc, object) => acc + object.quantity, 0));
+const valoreBuono = useLocalStorage("valore-buono", 8);
 
-const valoreBuono = ref(8);
-const totBuoni = computedEager(() => Math.floor(result.value / valoreBuono.value));
+const result = computed(() => articoliInseriti.value.reduce((acc, object) => acc + object.price, 0));
+const totBuoni = computed(() => Math.floor(result.value / valoreBuono.value));
 const spreco = computed(() => valoreBuono.value - (result.value % valoreBuono.value));
+
+const nameInserted = ref(false);
 
 const isMenuOpen = ref(false);
 const toggleMenu = useToggle(isMenuOpen);
 
-const coloreSpreco = computedEager(() => {
+const coloreSpreco = computed(() => {
     if (spreco.value > 7 && spreco.value <= 8) return "text-green-500";
     if (spreco.value > 6 && spreco.value <= 7) return "text-lime-500";
     if (spreco.value > 5 && spreco.value <= 6) return "text-yellow-300";
@@ -94,14 +112,26 @@ const coloreSpreco = computedEager(() => {
     if (spreco.value > 0 && spreco.value <= 1) return "text-red-500";
 });
 
-const input = ref();
-onMounted(() => inputFocus());
+const priceInput = ref();
+const nameInput = ref();
+onMounted(() => nameInputFocus());
 
-const addItem = () => {
-    if (newItem.value > 0) {
-        articoliInseriti.value.unshift({ id: counter.value, quantity: newItem.value });
+const addItem = async () => {
+    if (!nameInserted.value) {
+        if (newItem.name.length > 0) {
+            nameInserted.value = true;
+            await nextTick();
+            priceInputFocus();
+        }
+    }
+    if (newItem.price > 0) {
+        articoliInseriti.value.unshift({ id: counter.value, price: newItem.price, name: newItem.name });
         counter.value++;
-        newItem.value = null;
+        newItem.name = "";
+        newItem.price = null;
+        nameInserted.value = false;
+        await nextTick();
+        nameInputFocus();
     }
 };
 
@@ -116,11 +146,12 @@ const reset = () => {
     if (confirm("Sei sicuro di voler resettare la spesa?")) {
         articoliInseriti.value = [];
         counter.value = 0;
-        inputFocus();
+        nameInputFocus();
     }
 };
 
-const inputFocus = () => input.value.focus();
+const nameInputFocus = () => nameInput.value.focus();
+const priceInputFocus = () => priceInput.value.focus();
 
 const convertToCurrency = number =>
     number.toLocaleString("it-IT", {
